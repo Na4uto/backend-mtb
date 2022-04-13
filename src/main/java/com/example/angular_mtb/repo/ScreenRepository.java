385b0fe4a9d6e8f3a5709b5d7d404940fa51ce5b
Ubuntu <ubuntu@ip-172-31-33-99.ap-south-1.compute.internal>
2022-04-13 15:42:56 +0530

    screens added

---
+++ src/main/java/com/example/angular_mtb/repo/ScreenRepository.java
@@ -0,0 +1,10 @@
+package com.example.angular_mtb.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.angular_mtb.model.Screen;
+
+
+public interface ScreenRepository extends JpaRepository<Screen, Integer> {
+
+}
